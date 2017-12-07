e4f64c43c937a4f62fae985e9b6204e6982040df
Onur Eker <yonureker@gmail.com>
2017-12-07 01:06:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+def display_rainbow(array)
+  puts "#{array[0][0]}: #{array[0], array[1][0]}: #{array[1], array[2][0]}: #{array[2], array[3][0]}: #{array[3], array[4][0]}: #{array[4], array[5][0]}: #{array[5], array[6][0]}: #{array[6]}"
+end
