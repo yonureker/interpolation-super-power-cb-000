8a9c0777ee7bbcad36e67f7627e69d7d05d63e32
Onur Eker <yonureker@gmail.com>
2017-12-07 01:08:33 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(array)
-  puts "#{array[0][0].upcase}: #{array[0]}, #{array[1][0]}: #{array[1]}, #{array[2][0]}: #{array[2]}, #{array[3][0]}: #{array[3]}, #{array[4][0]}: #{array[4]}, #{array[5][0]}: #{array[5]}, #{array[6][0]}: #{array[6]}"
+  puts "#{array[0][0].upcase}: #{array[0]}, #{array[1][0].upcase}: #{array[1]}, #{array[2][0].upcase}: #{array[2]}, #{array[3][0].upcase}: #{array[3]}, #{array[4][0].upcase}: #{array[4]}, #{array[5][0].upcase}: #{array[5]}, #{array[6][0].upcase}: #{array[6]}"
 end
